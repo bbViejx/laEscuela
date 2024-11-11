65db6ce61509ff30572d858d4f25de721aa6d44b
bbviejx <aimedemi@gmail.com>
2024-11-11 00:22:08 -0300

    Strobo + frameRate control
    -on/off strobo
    -frameRate controllers

--- Arduino/pruebaLuz/pruebaLuz.ino
+++ Arduino/pruebaLuz/pruebaLuz.ino
@@ -1,30 +1,42 @@
 int led = 9;         // the PWM pin the LED is attached to
 int brightness = 0;  // how bright the LED is
 int fadeAmount = 5;
+bool ledin =true; 
 
 void setup() {
   Serial.begin(9600);
-  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
 }
 
 void loop() {
-  for(int fadeValue = 129; fadeValue<=255; fadeValue+-5){
-    analogWrite(8, fadeValue);
-    analogWrite(7, fadeValue);
-    delay(5);
-  }
+  //  // set the brightness of pin 9:
+  // analogWrite(9, brightness);
+  // analogWrite(6, brightness);
 
-  for(int fadeValue = 255 ; fadeValue >= 129 ; fadeValue -= 5){
-    analogWrite(7, fadeValue);
-    delay(5);
-  }
+  // // change the brightness for next time through the loop:
+  // brightness = brightness + fadeAmount;
 
+  // // reverse the direction of the fading at the ends of the fade:
+  // if (brightness <= 0 || brightness >= 255) {
+  //   fadeAmount = -fadeAmount;
+  // }
+  // // wait for 30 milliseconds to see the dimming effect
+  // delay(30);
 
-  //   char led = Serial.read();
-  //   if (led == '1') {
-  //     digitalWrite(8, HIGH);
-  //   } else if (led == '0') {
-  //     digitalWrite(8, LOW);
-  //   }
-   //}
+    char led = Serial.read();
+    if (led == '1') {
+      analogWrite(9, 255);
+      analogWrite(6, 255);
+    } else if (led == '2') {
+      if(ledin){
+      analogWrite(9, 0);
+      analogWrite(6, 0);
+      }
+      else{
+      analogWrite(9, 255);
+      analogWrite(6,255);
+      }
+      ledin = !ledin;
+    }
+   
 }
--- Processing/LucesArduino/luces_arduino/luces_arduino.pde
+++ Processing/LucesArduino/luces_arduino/luces_arduino.pde
@@ -5,21 +5,41 @@ import processing.serial.*;
 // The serial port:
 Serial myPort;
 boolean tuki = false;
+char modo = '1';
+int brightness = 255;
 
-void setup(){
-  
-  String portName = Serial.list()[1]; 
+void setup() {
+  size(600, 600);
+  String portName = Serial.list()[1];
   myPort = new Serial(this, portName, 9600);
-  frameRate(20);
+  frameRate(10);
 }
 
-void draw(){
-if(tuki){
-  myPort.write('1');
+void draw() {
+  myPort.write(modo);
+  myPort.write(brightness);
 }
-else{
-  myPort.write('0');
-}
-frameRate(frameRate-1);
-tuki = !tuki;
+
+void keyPressed() {
+  switch(key) {
+  case 'i':
+    modo = '2';
+    break;
+  case 'o':
+    modo = '1';
+    break;
+  case 'q':
+    brightness = brightness-5 <= 0 ? 0 :  brightness - 5;
+    break;
+  case 'w':
+    brightness = brightness+5 >=255 ? 255 : brightness + 5;
+  case 'a':
+    frameRate(frameRate -5 <= 1 ? 1 :  frameRate - 5);
+    break;
+  case 's':
+    frameRate(frameRate + 5 >= 255 ? 255 :  frameRate + 5);
+    break;
+  default:
+    break;
+  }
 }
